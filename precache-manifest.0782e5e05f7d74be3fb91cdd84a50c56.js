1c62cd01fdfa1a5af60e9565ba5066b934a94f70
ducnvph08993 <57821959+ducnvph08993@users.noreply.github.com>
2020-10-15 10:21:23 +0700

    Updates

---
+++ precache-manifest.0782e5e05f7d74be3fb91cdd84a50c56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b15f8044fda9a70a94755a8a1d6a937",
+    "url": "/task-4/index.html"
+  },
+  {
+    "revision": "5bcf00d7bb424037ecdf",
+    "url": "/task-4/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "43dc39c6d8cd51019d2e",
+    "url": "/task-4/static/css/main.9a1b5f59.chunk.css"
+  },
+  {
+    "revision": "5bcf00d7bb424037ecdf",
+    "url": "/task-4/static/js/2.7440cae4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/task-4/static/js/2.7440cae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43dc39c6d8cd51019d2e",
+    "url": "/task-4/static/js/main.9a1b317a.chunk.js"
+  },
+  {
+    "revision": "bfa02358ca7e9e8117e6",
+    "url": "/task-4/static/js/runtime-main.b3ed6192.js"
+  }
+]);
